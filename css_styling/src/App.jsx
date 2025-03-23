e21a5467cb6400d28d43f6e2a3c489402c8a572a
Nyabayo <ernestosindo9@gmail.com>
2025-03-23 14:57:03 +0300

    css styling

---
+++ css_styling/src/App.jsx
@@ -0,0 +1,18 @@
+//How to add styles to components with css
+//1. external
+//2. modules
+//3. inline
+
+import Button from "./Button/Button.jsx";
+
+function App() {
+
+
+  return (
+    <>
+        <Button></Button>
+    </>
+  )
+}
+
+export default App
