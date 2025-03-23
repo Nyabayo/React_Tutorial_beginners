a7647d731a068a91bdbcf6b92f2485ad04653e4c
Nyabayo <ernestosindo9@gmail.com>
2025-03-23 12:22:25 +0300

    introduction_react

---
+++ introduction_react/src/App.jsx
@@ -0,0 +1,17 @@
+
+import Header from "./Header.jsx";
+import Footer from "./Footer.jsx";
+import Food from "./Food.jsx";
+
+function App() {
+    return(
+        //Fragment <></>
+        <>
+        <Header />
+            <Food />
+        <Footer />
+        </>
+    )
+}
+
+export default App
